b0d611915bd4402c88f334a307ef4ef8ae2629da
dylancmiller <riderider4@yahoo.com>
2013-12-23 22:08:53 -0600

    Added starred playlist to the playlist chooser
    -starred playlist is displayed at the top of the list
    -refactored code for making a playlistChooserItem.

--- scripts/playlist-chooser.js
+++ scripts/playlist-chooser.js
@@ -9,48 +9,68 @@
     var doPlaylistChooser = function () {
         document.getElementById('playlistChooser').innerHTML = '';
         var curLibrary = Library.forCurrentUser();
+        document.getElementById('playlistChooser').appendChild(createPlaylistChooserItem(models, Library, Image, curLibrary.starred, true));
+
         curLibrary.playlists.snapshot().done(function (snapshot) {
             for (var i = 0, l = snapshot.length; i < l; i++) {
-                var playlistContainer = document.createElement('div');
-                playlistContainer.className = 'playlistChooser-item';
-                if (i % 2 == 1)
-                    playlistContainer.className += ' playlistChooser-item-alt';
-                var val = $('playlistChooser', 'home');
-                document.getElementById('playlistChooser').appendChild(playlistContainer);
-
                 var curPlaylist = snapshot.get(i);
-                var image = Image.forPlaylist(curPlaylist, { width: 150, height: 150 });
-                
-                var coverContainer = document.createElement('div');
-                coverContainer.className = 'albumCover';
-                coverContainer.appendChild(image.node);
-
-                var coverLink = document.createElement('a');
-                coverLink.href = '#';
-                coverLink.id = curPlaylist.uri;
-                $(coverLink).click(function (obj) {
-                    models.application.openURI('spotify:app:powerhour-app:play:' + this.id);
-                });
-                coverLink.appendChild(coverContainer);
-                playlistContainer.appendChild(coverLink);
-
-                var detailsContainer = document.createElement('div');
-                detailsContainer.className = 'playlistDetails-container';
-                playlistContainer.appendChild(detailsContainer);
-                
-                var name = document.createElement('span');
-                name.className = 'playlistName';
-                name.innerText = curPlaylist.name.decodeForText();
-                detailsContainer.appendChild(name);
-
-                var descr = document.createElement('span');
-                descr.className = 'playlistDescription';
-                descr.innerText = curPlaylist.description.decodeForText();
-                detailsContainer.appendChild(descr);
-                
+                var isAlternate = i % 2 == 1;
+                document.getElementById('playlistChooser').appendChild(createPlaylistChooserItem(models, Library, Image, curPlaylist, isAlternate));
             }
         });
     }
 
     exports.doPlaylistChooser = doPlaylistChooser;
 });
+
+function createPlaylistChooserItem(models, Library, Image, playlist, isAlternate) {
+    var playlistContainer = document.createElement('div');
+    playlistContainer.className = 'playlistChooser-item';
+    if (isAlternate)
+        playlistContainer.className += ' playlistChooser-item-alt';
+    var val = $('playlistChooser', 'home');
+    //document.getElementById('playlistChooser').appendChild(playlistContainer);
+
+    //var curPlaylist = snapshot.get(i);
+    var image = Image.forPlaylist(playlist, { width: 150, height: 150 });
+                
+    var coverContainer = document.createElement('div');
+    coverContainer.className = 'albumCover';
+    coverContainer.appendChild(image.node);
+
+    var coverLink = document.createElement('a');
+    coverLink.href = '#';
+    coverLink.id = playlist.uri;
+    $(coverLink).click(function (obj) {
+        models.application.openURI('spotify:app:powerhour-app:play:' + this.id);
+    });
+    coverLink.appendChild(coverContainer);
+    playlistContainer.appendChild(coverLink);
+
+    var detailsContainer = document.createElement('div');
+    detailsContainer.className = 'playlistDetails-container';
+    playlistContainer.appendChild(detailsContainer);
+                
+    var name = document.createElement('span');
+    name.className = 'playlistName';
+    if (playlist.name == null) {
+        //Assume this is the starred playlist
+        name.innerText = 'Starred';
+    }
+    else {
+        name.innerText = playlist.name.decodeForText();
+    }
+    detailsContainer.appendChild(name);
+
+    var descr = document.createElement('span');
+    descr.className = 'playlistDescription';
+    if (playlist.description == null) {
+        descr.innerText = '';
+    }
+    else {
+        descr.innerText = playlist.description.decodeForText();
+    }
+    detailsContainer.appendChild(descr);
+
+    return playlistContainer;
+} 
