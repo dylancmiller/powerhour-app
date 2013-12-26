9179e800aa17e9236518220fbbfb2f6f44bdc790
johnnymcabe <riderider4@yahoo.com>
2013-12-26 12:13:38 -0600

    Fixed timer issue
    -The timer is only started when a song is first paused and then played
    again.
    -There is still an intermittent bug that causes multiple timers to be
    added to the window.

--- scripts/play.js
+++ scripts/play.js
@@ -7,7 +7,8 @@
     'use strict';
 
     var counter;
-    var tempPlaylistURI;
+    var tempPlaylist;
+    var curTime = 10;
 
     var doPlay = function (args) {
         //Setup playlist and play controls
@@ -21,7 +22,7 @@
             //Create temp playlist
             models.Playlist.createTemporary(origPlaylist.name).done(function (_playlist) {
                 _playlist.load('tracks').done(function (playlist) {
-                    tempPlaylistURI = playlist.uri;
+                    tempPlaylist = playlist;
                     //Get original tracks
                     origPlaylist.tracks.snapshot().done(function (origTracksSS) {
                         origTracksSS.loadAll('name', 'artists', 'album', 'duration').done(function (origTracks) {
@@ -45,13 +46,17 @@
                                     counterContainer.innerHTML = '<span class="counter-title">Seconds till next song:</span>';
                                     counter = document.createElement('div');
                                     counter.id = 'counter';
-                                    //counter.innerText = '60';
-                                    counter.innerText = '10';
+                                    counter.innerText = curTime;
 
-                                    models.player.addEventListener('change', stopStartTimer);
+                                    models.player.addEventListener('change', genericChange);
+                                    models.player.addEventListener('change:track', trackChanged);
+                                    models.player.addEventListener('change:playing', playingChanged);
+                                    models.player.addEventListener('change:context', contextChanged);
 
                                     counterContainer.appendChild(counter);
                                     document.getElementById('playControls').appendChild(counterContainer);
+
+                                    //models.player.playContext(tempPlaylist);
                                 });
                             });
                         });
@@ -62,43 +67,99 @@
     };
 
     exports.doPlay = doPlay;
+    
+    function genericChange() {
+        var val = 0;
+    }
 
-    var timerId = -1;
-    function stopStartTimer() {
+    function trackChanged() {
+        curTime = 10;
+        counter.innerText = curTime;
+    }
+
+    var timerId = null;
+    function playingChanged() {
         models.player.load('context', 'playing').done(function (player) {
-            if (player.playing && player.context.uri == tempPlaylistURI) {
-                if (timerId = -1) {
+            if (player.context.uri == tempPlaylist.uri) {
+                if (player.playing) {
+                    //Player was previously paused. Restart timer.
                     timerId = setInterval(updateTimerView, 1000);
                 }
-            }
-            else {
-                clearInterval(timerId);
-                timerId = -1;
+                else {
+                    //Player is paused
+                    clearInterval(timerId);
+                }
             }
         });
-    };
+    }
+
+    function contextChanged() {
+        models.player.load('context').done(function (player) {
+            if (player.context.uri != tempPlaylist.uri) {
+                //User is playing a different context. Delete temp playlist.
+                models.Playlist.removeTemporary(tempPlaylist);
+                clearInterval(timerId);
+            }
+        });
+    }
+
+    //
+    //This block of code SHOULD work if change:context is fired correctly
+    //
+    //var timerId = null;
+    //function playingChanged() {
+    //    models.player.load('context', 'playing').done(function (player) {
+    //        if (player.context.uri == tempPlaylist.uri) {
+    //            if (player.playing) {
+    //                if (timerId != null) {
+    //                    //Player was previously paused. Restart timer.
+    //                    timerId = setInterval(updateTimerView, 1000);
+    //                }
+    //            }
+    //            else {
+    //                //Player is paused
+    //                clearInterval(timerId);
+    //            }
+    //        }
+    //    });
+    //}
+
+    //function contextChanged() {
+    //    models.player.load('context').done(function (player) {
+    //        if (player.context.uri == tempPlaylist.uri) {
+    //            //User started playing this context. Start timer.
+    //            //timerId = setInterval(updateTimerView, 1000);
+    //        }
+    //        else {
+    //            //User is playing a different context. Delete temp playlist.
+    //            models.Playlist.removeTemporary(tempPlaylist);
+    //            clearInterval(timerId);
+    //        }
+    //    });
+    //}
 
     function updateTimerView() {
-        models.player.load('context', 'playing').done(function (player) {
-            if (player.playing && player.context.uri == tempPlaylistURI) {
-                var curTime = parseInt(counter.innerText);
-                if (curTime == 0) {
-                    player.skipToNextTrack();
-                    var duration = models.player.track.duration;
-                    var start = 0;
-                    if (duration > 60000) {
-                        start = (duration - 60000) / 2;
+        models.player.load('context').done(function (player) {
+            models.player.load('context', 'playing').done(function (player) {
+                if (player.context.uri == tempPlaylist.uri) {
+                    if (curTime <= 0) {
+                        player.skipToNextTrack();
+                        var duration = models.player.track.duration;
+                        var start = 0;
+                        if (duration > 60000) {
+                            start = (duration - 60000) / 2;
+                        }
+                        player.seek(start).done(function () {
+                            //curTime = 60;
+                            curTime = 10;
+                        });
                     }
-                    player.seek(start).done(function () {
-                        //curTime = 61;
-                        curTime = 11;
-                        counter.innerText = curTime - 1;
-                    });
+                    else {
+                        curTime--;
+                    }
+                    counter.innerText = curTime;
                 }
-                else {
-                    counter.innerText = curTime - 1;
-                }
-            }
+            });
         });
     }
 });
